0bb9de7da4b73a01bcd515eede8a93b1282b816a
Supriya Negi <30532477+chickennugget1@users.noreply.github.com>
2020-12-12 18:28:55 +0530

    Update 8Dec.cpp

--- Week 2/8Dec.cpp
+++ Week 2/8Dec.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& nums) {
+        int cnt = 0;
+        unordered_map<int, int>m;
+        for(auto it: nums)
+        {
+            if(it % 60 == 0)
+                cnt += m[0];
+            else
+                cnt += m[60 - (it % 60)];
+            m[it % 60]++;
+        }
+        return cnt;
+    }
+};
